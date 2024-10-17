3a5a0f486a76d32033bbbeed20ffd45ff5cae1b4
emel.elias <emelelias@gmail.com>
2024-10-17 09:51:34 +0200

    initial commit

---
+++ PokemonFetcher/PokemonFetcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonFetcherApp.swift
+//  PokemonFetcher
+//
+//  Created by Emel Elias on 2024-10-17.
+//
+
+import SwiftUI
+
+@main
+struct PokemonFetcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
