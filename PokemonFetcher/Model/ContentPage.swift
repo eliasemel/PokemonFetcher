77a98ed3cb66cd59bf982ecc3e1c4dd1401c6872
emel.elias <emelelias@gmail.com>
2024-10-17 10:18:59 +0200

    modified the service layer

---
+++ PokemonFetcher/Model/ContentPage.swift
@@ -0,0 +1,8 @@
+//
+//  ContentPage.swift
+//  PokemonFetcher
+//
+//  Created by Emel Elias on 2024-10-17.
+//
+
+import Foundation
---
+++ PokemonFetcher/Service/DefaultPokemonService.swift
@@ -0,0 +1,8 @@
+//
+//  DefaultPokemonService.swift
+//  PokemonFetcher
+//
+//  Created by Emel Elias on 2024-10-17.
+//
+
+import Foundation
