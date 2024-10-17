66c0368e929eb39c95766e23e271f352cab57f69
emel.elias <emelelias@gmail.com>
2024-10-17 10:13:16 +0200

    added service class

---
+++ PokemonFetcher/Service/PokemonService.swift
@@ -0,0 +1,12 @@
+//
+//  PokemonService.swift
+//  PokemonFetcher
+//
+//  Created by Emel Elias on 2024-10-17.
+//
+
+import Foundation
+
+protocol PokemonService {
+    func fetchPokemons() async
+}
